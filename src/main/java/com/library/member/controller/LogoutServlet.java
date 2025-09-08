19f44405c0f90325c234681448f2c15fc4dae34a
hojun <user1@DESKTOP-5R85Q1T>
2025-09-08 17:45:22 +0900

    [ADD] 로그아웃 추가
    1. 로그아웃 서블릿 추가(세션 파괴)

--- src/main/java/com/library/member/controller/LogoutServlet.java
+++ src/main/java/com/library/member/controller/LogoutServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -26,16 +28,11 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		if(session != null) {
+			session.invalidate(); // 세션 파괴 -> 다시 로그인
+			response.sendRedirect("/");
+		}
 	}
 
 }
