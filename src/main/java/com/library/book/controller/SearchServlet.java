9deb76d292d7a09651a16ba4a9c9f24036aa23a5
user1 <user1@DESKTOP-LGIEHMH>
2025-09-09 11:31:42 +0900

    [ADD] java파일 페이지 연결 밑작업 1. doGet 메소드에 request.getRequestDispatcher().forward() 페이지 연결

--- src/main/java/com/library/book/controller/SearchServlet.java
+++ src/main/java/com/library/book/controller/SearchServlet.java
@@ -1,11 +1,12 @@
 package com.library.book.controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class searchServlet
@@ -25,17 +26,16 @@ public class SearchServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.getRequestDispatcher("/WEB-INF/views/book/search.jsp")
+		.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
